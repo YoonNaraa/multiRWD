c96f7654e5ffb6cb316d61047ef14ff71abc60b1
YoonNaraa <ynr0107@gmail.com>
2019-03-12 16:36:08 +0900

    메인 메뉴의 하위 메뉴 제어 함수

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,55 @@
-var viewport = matchMedia("(max-width:999px)");
+var viewport = matchMedia('(max-width:999px)');
 
 if (viewport.matches) {
-  var nav = $(".navigation");
-  var btn = $(".btn-menubar");
+  var nav = $('.navigation');
+  var btn = $('.btn-menubar');
+  var menu = $('.menu');
+  var lists = $('.menu-list');
+  var items = $('.menu-item');
+  var sub = $('.sub-menu');
 
-  btn.on("click", function(e) {
+  lists.attr('aria-haspopup', 'true');
+  lists.attr('aria-expanded', 'false');
+  items.attr('tabindex', '0');
+  items.addClass('icon-plus');
+
+  // 메인메뉴 제어 버튼 
+  items.on('click', function(e){
     e.preventDefault();
-    nav.toggleClass("menu-act");
-    if (nav.hasClass("menu-act")) {
-      btn.attr("aria-label", "메인메뉴 닫기");
+    sub.removeClass('sub-menu-show');
+    lists.attr('aria-expanded', 'false');
+    $(this).siblings().addClass('sub-menu-show');
+    $(this).parent().attr('aria-expanded', 'true');
+  });
+
+  // 메뉴 항목 선택 시 하위메뉴 제어
+  btn.on('click', function(e) {
+    e.preventDefault();
+    nav.toggleClass('menu-act');
+    if (nav.hasClass('menu-act')) {
+      btn.attr('aria-label', '메인메뉴 닫기');
     } else {
-      btn.attr("aria-label", "메인메뉴 열기");
+      btn.attr('aria-label', '메인메뉴 열기');
     }
+  });
+
+  items.on('click keyup', function(e){
+    e.preventDefault();
+    if(e.type === 'click' || (e.type === 'keyup' && e.keyCode === 13)){
+      sub.removeClass('sub-menu-show');
+      lists.attr('aria-expanded', 'false');
+      items.addClass('icon-plus');
+      $(this).removeClass('icon-plus').addClass('icon-minus');
+      $(this).siblings().addClass('sub-menu-show');
+      $(this).parent().attr('aria-expanded', 'true');
+    }    
+  });
+
+  // 키보드 포커스가 메뉴를 벗어나면 메인메뉴 감추기
+  menu.on('focusout', function(){
+    nav.removeClass('menu-act');
+  });
+  menu.on('focusin', function(){
+    nav.addClass('menu-act');
   });
 }
